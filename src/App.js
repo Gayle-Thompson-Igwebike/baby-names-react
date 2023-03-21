62fb93d405191a79dc67d1afacd3bc743413d006
Gayle Ngozi <gaylengozi@gmail.com>
2023-03-21 22:17:51 +0000

    added a reset button after search.

--- src/App.js
+++ src/App.js
@@ -4,24 +4,34 @@ import "./App.css";
 
 function App() {
   const [allBabyNames, setAllBabyNames] = useState([]);
-  const [searchedNames, setSearchedNames] = useState([]);
+  const [searchedNames, setSearchedNames] = useState("");
+  const [favouriteNames, setFavouriteNames] = useState([]);
 
   useEffect(() => {
     setAllBabyNames(babyNames);
-
   }, []);
 
-  const handleSearch = event => {
+  const handleSearch = (event) => {
     setSearchedNames(event.target.value);
   };
 
+  const resetSearch = (event) => {
+    setSearchedNames("");
+  };
+
   const filteredBabyNames = allBabyNames.filter((baby) => {
-    return (
-      baby.name.toLowerCase().includes(searchedNames)
-    
-    );
+    return baby.name.toLowerCase().includes(searchedNames);
   });
-  
+
+  const handleRandomName = () => {
+    const randomNameIndex = Math.floor(Math.random() * allBabyNames.length);
+    const randomName = allBabyNames[randomNameIndex].name;
+    setSearchedNames(randomName);
+  };
+  const handleAddToFavourites = (name) => {
+    setFavouriteNames((favourites) => [...favourites, name]);
+  };
+
   return (
     <header>
       <h1> BABY NAME-PICKER APP</h1>
@@ -33,7 +43,7 @@ function App() {
         ></img>
       </div>
       <div className="search">
-        <p>Find baby names of your choice...</p>
+        <p>Find baby names of your choice below...</p>
         <input
           value={searchedNames}
           className="search-bar"
@@ -42,13 +52,30 @@ function App() {
           placeholder="Pick or enter a name"
           onChange={handleSearch}
         ></input>
-        <button type="button" id="btn" onClick={handleSearch}>Search Names</button>
+      </div>
+      <div className="btn1and2">
+        <button type="button" id="btn" onClick={resetSearch}>
+          Reset
+        </button>
+        <button
+          className="random-name-button"
+          type="button"
+          id="btn"
+          onClick={handleRandomName}
+        >
+          Generate A Random Name
+        </button>
       </div>
       <div className="cards-display">
         {filteredBabyNames
           .sort((a, b) => a.name.localeCompare(b.name))
           .map((baby) => (
-            <div className={`name-cards color-${baby.sex}`}>{baby.name}</div>
+            <div className={`name-cards color-${baby.sex}`}>
+              {baby.name}{" "}
+              <button id="fave-btn" onClick={() => handleAddToFavourites(baby.name)}>
+                Add To Faves
+              </button>
+            </div>
           ))}
       </div>
     </header>
