c8541c40ba79241f07da5a5d613de8a6a0e1b6da
Gayle Ngozi <gaylengozi@gmail.com>
2023-11-01 03:40:01 +0000

    update to css

--- src/App.js
+++ src/App.js
@@ -29,18 +29,30 @@ function App() {
     const randomName = allBabyNames[randomNameIndex].name;
     setSearchedNames(randomName);
   };
-  const handleAddToFavourites = (name) => {
-    setFavouriteNames((favourites) => [...favourites, name]);
-  };
+  // const handleAddToFavourites = (name) => {
+  //   setFavouriteNames((favourites) => [...favourites, name]);
+    const handleAddToFavourites = (name) => {
+      if (favouriteNames.includes(name)) {
+        // If the name is already a favorite, remove it
+        setFavouriteNames((favourites) =>
+          favourites.filter((favourite) => favourite !== name)
+        );
+      } else {
+        // If the name is not in favorites, add it
+        setFavouriteNames((favourites) => [...favourites, name]);
+      }
+    };
+
+  
 
   return (
     <header>
-      <h1> BABY NAMEs-PICKER APP</h1>
+      <h1> BABY NAME-PICKER</h1>
 
       <div className="img-container">
         <img
           src="https://www.pngitem.com/pimgs/m/234-2341738_stork-png-transparent-background-transparent-background-stork-cartoon.png"
-          alt="stork and baby"
+          alt="stork and baby img"
         ></img>
       </div>
       <div className="search">
@@ -67,14 +79,30 @@ function App() {
           Generate Random Name
         </button>
       </div>
+
+      <div className="favourite-names">
+        <h2>Chosen Favourite Names:</h2>
+        <ul>
+          {favouriteNames.map((name, index) => (
+            <li key={index}>{name}</li>
+          ))}
+        </ul>
+      </div>
+
       <div className="cards-display">
         {filteredBabyNames
           .sort((a, b) => a.name.localeCompare(b.name))
           .map((baby) => (
             <div className={`name-cards color-${baby.sex}`}>
               {baby.name}{" "}
-              <button id="fave-btn" onClick={() => handleAddToFavourites(baby.name)}>
-                Add
+              <button
+                id="fave-btn"
+                className={
+                  favouriteNames.includes(baby.name) ? "favourited" : ""
+                }
+                onClick={() => handleAddToFavourites(baby.name)}
+              >
+                {favouriteNames.includes(baby.name) ? "Remove" : "Add"}
               </button>
             </div>
           ))}
